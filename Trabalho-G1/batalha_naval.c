836440e59bed43d34362c81ca32901497773d57b
passosleo <leonardopassos98@gmail.com>
2022-05-16 22:01:12 -0300

    horizontal and vertical validations

--- Trabalho-G1/batalha_naval.c
+++ Trabalho-G1/batalha_naval.c
@@ -41,8 +41,8 @@ void delay(unsigned int secs) {
 }
 
 int main(void) {
-  char column, direction;
-  int ships = 3, row;
+  char column = ' ', direction = ' ';
+  int ships = 3, row = 0, index_limit = 0;
   short player1 = 1, is_valid = 0;
 
   printf("____________________________________\n\n");
@@ -74,10 +74,11 @@ int main(void) {
     printf("\nNavios restantes: %d\n\n", ships);
 
     do{
+      //Escolha do sentido
       printf("\nEscolha o sentido do navio\n[H] para Horizontal\n[V] para Vertical\n");
       fflush(stdin);
       fgets(&direction, 50, stdin);
-      
+
       if(isalpha(direction)){
         direction = toupper(direction);
         if(direction == 'H' || direction == 'V'){
@@ -91,16 +92,22 @@ int main(void) {
     }while(!is_valid);
 
     is_valid = 0;
-    
+
+    //Escolha da coluna
     do{
       printf("\nEscolha a coluna: ");
       fflush(stdin);
       fgets(&column, 50, stdin);
-      
+
+      //Valida se é letra, se direção for horizontal limita em F (index_limit)
       if(isalpha(column)){
         column = toupper(column);
-        
-        for(int c = 0; c < 10; c++){
+        if(direction == 'H'){
+          index_limit = 6;
+        }else{
+          index_limit = 10;
+        }
+        for(int c = 0; c < index_limit; c++){
           if(column == columns[c]){
             is_valid = 1;
           };
@@ -109,22 +116,33 @@ int main(void) {
   
       if(!is_valid){
         printf("\nLocal inválido!\n");
-      }
+      };
     }while(!is_valid);
   
     is_valid = 0;
-  
+
+    //Escolha da linha
     do{
       printf("\nEscolha a linha: ");
       scanf("%d%*c", &row);
-      
-      if(!isalpha(row) || row >= 1 && row <= 10){
-        is_valid = 1;
-      }
+
+      //Valida se é número, se direção for horizontal limita na coluna F (index_limit)
+      if(!isalpha(row)){
+        if(direction == 'V'){
+          index_limit = 6;
+        }else{
+          index_limit = 10;
+        };
+    
+        if(row >= 1 && row <= index_limit){
+          is_valid = 1;
+        };
+      };
+       
   
       if(!is_valid){
         printf("\nLocal inválido!\n");
-      }
+      };
     }while(!is_valid);  
   
     printf("\n\nNavio posicionado com sucesso!\n\n");
@@ -132,7 +150,9 @@ int main(void) {
     ships--;
     delay(2);
     clearScreen();
-        printf("\n\nDEBUGGER\n\ndirection: %c\ncolumn: %c\nrow: %d\n\n",direction,column,row);
+    
+    //debugger
+    printf("\n\nDEBUGGER\n\ndirection: %c\ncolumn: %c\nrow: %d\n\n",direction,column,row);
     
   }while(ships != 0);
   
